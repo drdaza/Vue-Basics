9c9281dc38f28411d52d3063970b66ab15032cf6
David Daza <drdaza18@gmail.com>
2022-12-06 17:24:43 +0100

    added  exersises

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, computed } from 'vue';
+    const password = ref('password');
+   
+    function changePass(){
+        (password.value == `password`) ? password.value = 'name': password.value = 'password';
+    }
+    
+</script>
+
+<template>
+    <div class="container text-center input-password">
+        <h1 class="my-3">Exercise Input Password</h1>
+        <input :type="password" name="" id="password" class="form-control">
+        <div class="d-block">
+            <span id="show-password" class="mt-2 btn btn-primary" @click="changePass()">Show Password</span>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    #show-password:hover {
+        cursor: pointer;
+        user-select: none;
+    }
+</style>
---
+++ src/components/ReadMore.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, computed } from "vue";
+  let count = ref(0);
+  const title = ref("Exercise Read More");
+
+  function changeClass(){
+    (count.value == 0) ? count.value = 1 : count.value = 0;
+  }
+  
+  let change = computed(()=>{
+    return{
+        'cut-text': count.value == 0,
+        '': count.value == 1
+    }
+  })
+  
+
+</script>
+
+<template>
+  <div class="container text-center">
+    <h1 class="my-3">{{ title }}</h1>
+    <div>
+      <p :class="['mb-3', 'd-block',change]">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
+        molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
+        deserunt aperiam maxime consectetur recusandae quasi commodi dicta
+        temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
+        quaerat autem non hic, quidem dolore laboriosam.
+      </p>
+      <button class="btn btn-primary" @click="changeClass()">Show More</button>
+    </div>
+    <hr class="mb-4" />
+  </div>
+</template>
+
+<style lang="css"></style>
